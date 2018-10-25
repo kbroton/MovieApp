bab0bfcd43a7fce609846fcb9d788506c50e8ef8
Kelly Broton <k.broton@live.com>
2018-10-25 10:24:33 -0700

    add files

---
+++ MovieDb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MovieDb.Global" Language="C#" %>
---
+++ MovieDb/MovieDbApp/App.js
@@ -0,0 +1,5 @@
+﻿(function = () {
+    var app = angular.module('App', ['ngRoute']);
+
+    app.controller('MovieCtrl', MovieCtrl);
+})();
